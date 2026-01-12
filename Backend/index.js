4899c724c4df0e7e93d529ba79aeb580f659b8a7
RupakRigic <155940247+RupakRigic@users.noreply.github.com>
2026-01-12 17:11:42 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import e from "express";
+const app = e();
+const port = 3000;
+
+app.get("/", (req, res) => {
+
+ const users = {
+    'name': 'John',
+    'age': 30,
+    'city': 'New York'
+    };
+  
+    res.send(users);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+export default app;
+
